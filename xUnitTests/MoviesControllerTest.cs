3a6cf992dd5e681a0e548adb6c9c500b277496b3
Unknown <kokaruk@gmail.com>
2019-04-18 00:08:01 +1000

    GetAll uTests

--- xUnitTests/MoviesControllerTest.cs
+++ xUnitTests/MoviesControllerTest.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
@@ -29,55 +31,192 @@ namespace TestProject1
         public async void MoviesController_GetAll_ReturnOkResultOfFullCollection()
         {
             // arrange
-
             const string endPoint = "movies";
 
             var fakeMoviesList = new List<Movie>
-                {new Movie {Id = "1", Title = "T1"}, new Movie {Id = "2", Title = "T2"}};
+            {
+                new Movie {Id = "1", Title = "T1"}, 
+                new Movie {Id = "2", Title = "T2"}
+            };
+            
             var moqMovieRepository = new Mock<IMovieRepository>();
-            moqMovieRepository.Setup(rep => rep.GetAllAsync(endPoint)).ReturnsAsync(fakeMoviesList);
+            moqMovieRepository.Setup(repo => repo.GetAllAsync(endPoint)).ReturnsAsync(fakeMoviesList);
 
-            var movieRepoFake = new Dictionary<string, IMovieRepository> {{"rep", moqMovieRepository.Object}};
-
-
+            var movieRepoDictFake = new Dictionary<string, IMovieRepository> {{"rep", moqMovieRepository.Object}};
             var apiServiceMoq = new Mock<IApiService>();
             var paginationOptionsStub = new PaginationOptions();
 
-            apiServiceMoq.SetupGet(ms => ms.MovieServices).Returns(movieRepoFake);
+            apiServiceMoq.SetupGet(ms => ms.MovieServices).Returns(movieRepoDictFake);
 
             var posterServiceMoq = new Mock<IPosterService>();
             var optionsMoq = new Mock<IOptions<PaginationOptions>>();
             optionsMoq.SetupGet(i => i.Value).Returns(paginationOptionsStub);
 
-
             var moviesController =
                 new MoviesController(apiServiceMoq.Object, posterServiceMoq.Object, optionsMoq.Object);
 
-//            // act
+            // act
             var result = await moviesController.GetAll();
 
-//            //assert
+            //assert
             Assert.IsType<OkObjectResult>(result.Result);
 
             var response =
                 Assert.IsAssignableFrom<PageCollectionResponse<Movie>>(((OkObjectResult) result.Result).Value);
-            
+
             Assert.Equal(2, response.Items.Count());
+
+            Assert.Null(response.NextPage);
+            Assert.Null(response.PreviousPage);
         }
 
         [Fact]
-        public void MoviesController_GetAll_ReturnOkResultPaginated()
+        public async void MoviesController_GetAll_ReturnOkResultPaginated()
         {
+            // arrange
+            const string endPoint = "movies";
+
+            var fakeMoviesList = new List<Movie>
+            {
+                new Movie {Id = "1", Title = "T1"}, 
+                new Movie {Id = "2", Title = "T2"}
+            };
+            
+            var moqMovieRepository = new Mock<IMovieRepository>();
+            moqMovieRepository.Setup(rep => rep.GetAllAsync(endPoint)).ReturnsAsync(fakeMoviesList);
+
+            var movieRepoDictFake = new Dictionary<string, IMovieRepository> {{"rep", moqMovieRepository.Object}};
+
+            var apiServiceMoq = new Mock<IApiService>();
+            var paginationOptionsStub = new PaginationOptions {ItemsLimit = 1};
+
+            apiServiceMoq.SetupGet(ms => ms.MovieServices).Returns(movieRepoDictFake);
+
+            var posterServiceMoq = new Mock<IPosterService>();
+            posterServiceMoq.Setup(p => p.GetAsync("some title", "some year"))
+                .ReturnsAsync("fake url");
+            var optionsMoq = new Mock<IOptions<PaginationOptions>>();
+            optionsMoq.SetupGet(i => i.Value).Returns(paginationOptionsStub);
+
+            var urlHelperMoq = new Mock<IUrlHelper>(MockBehavior.Strict);
+            urlHelperMoq
+                .Setup(
+                    x => x.Action(
+                        It.IsAny<UrlActionContext>()
+                    )
+                )
+                .Returns("https://staging.kokaruk.com/")
+                .Verifiable();
+
+            var moviesController =
+                new MoviesController(apiServiceMoq.Object, posterServiceMoq.Object, optionsMoq.Object)
+                {
+                    Url = urlHelperMoq.Object, ControllerContext = {HttpContext = new DefaultHttpContext()}
+                };
+
+            // act
+            var result1 = await moviesController.GetAll(page: 1);
+            var result2 = await moviesController.GetAll(page: 2);
+
+            // assert
+            Assert.IsType<OkObjectResult>(result1.Result);
+            Assert.IsType<OkObjectResult>(result2.Result);
+            
+            var response1 =
+                Assert.IsAssignableFrom<PageCollectionResponse<Movie>>(((OkObjectResult) result1.Result).Value);
+            var response2 =
+                Assert.IsAssignableFrom<PageCollectionResponse<Movie>>(((OkObjectResult) result2.Result).Value);
+            
+            Assert.Single(response1.Items);
+            Assert.Single(response2.Items);
+            
+            Assert.Null(response1.PreviousPage);
+            Assert.NotNull(response1.NextPage);
+            Assert.NotNull(response2.PreviousPage);
+            Assert.Null(response2.NextPage);
         }
 
         [Fact]
-        public void MoviesController_GetAll_ReturnNoContentNoMoviesForAllRequest()
+        public async void MoviesController_GetAll_ReturnNoContentNoMoviesForAllRequest()
         {
+            // arrange
+            const string endPoint = "movies";
+
+            // ReSharper disable once CollectionNeverUpdated.Local
+            var fakeMoviesList = new List<Movie>();
+            
+            var moqMovieRepository = new Mock<IMovieRepository>();
+            moqMovieRepository.Setup(repo => repo.GetAllAsync(endPoint)).ReturnsAsync(fakeMoviesList);
+
+            var movieRepoDictFake = new Dictionary<string, IMovieRepository> {{"rep", moqMovieRepository.Object}};
+            var apiServiceMoq = new Mock<IApiService>();
+            var paginationOptionsStub = new PaginationOptions();
+
+            apiServiceMoq.SetupGet(ms => ms.MovieServices).Returns(movieRepoDictFake);
+
+            var posterServiceMoq = new Mock<IPosterService>();
+            var optionsMoq = new Mock<IOptions<PaginationOptions>>();
+            optionsMoq.SetupGet(i => i.Value).Returns(paginationOptionsStub);
+
+            var moviesController =
+                new MoviesController(apiServiceMoq.Object, posterServiceMoq.Object, optionsMoq.Object);
+
+            // act
+            var result = await moviesController.GetAll();
+
+            //assert
+            Assert.IsType<NoContentResult>(result.Result);
         }
 
         [Fact]
-        public void MoviesController_GetAll_ReturnNoContentNoMoviesPaginated()
+        public async void MoviesController_GetAll_ReturnNoContentNoMoviesPaginated()
         {
+            // arrange
+            const string endPoint = "movies";
+
+            var fakeMoviesList = new List<Movie>
+            {
+                new Movie {Id = "1", Title = "T1"}, 
+                new Movie {Id = "2", Title = "T2"}
+            };
+            
+            var moqMovieRepository = new Mock<IMovieRepository>();
+            moqMovieRepository.Setup(rep => rep.GetAllAsync(endPoint)).ReturnsAsync(fakeMoviesList);
+
+            var movieRepoDictFake = new Dictionary<string, IMovieRepository> {{"rep", moqMovieRepository.Object}};
+
+            var apiServiceMoq = new Mock<IApiService>();
+            var paginationOptionsStub = new PaginationOptions {ItemsLimit = 1};
+
+            apiServiceMoq.SetupGet(ms => ms.MovieServices).Returns(movieRepoDictFake);
+
+            var posterServiceMoq = new Mock<IPosterService>();
+            posterServiceMoq.Setup(p => p.GetAsync("some title", "some year"))
+                .ReturnsAsync("fake url");
+            var optionsMoq = new Mock<IOptions<PaginationOptions>>();
+            optionsMoq.SetupGet(i => i.Value).Returns(paginationOptionsStub);
+
+            var urlHelperMoq = new Mock<IUrlHelper>(MockBehavior.Strict);
+            urlHelperMoq
+                .Setup(
+                    x => x.Action(
+                        It.IsAny<UrlActionContext>()
+                    )
+                )
+                .Returns("https://staging.kokaruk.com/")
+                .Verifiable();
+
+            var moviesController =
+                new MoviesController(apiServiceMoq.Object, posterServiceMoq.Object, optionsMoq.Object)
+                {
+                    Url = urlHelperMoq.Object, ControllerContext = {HttpContext = new DefaultHttpContext()}
+                };
+
+            // act
+            var result = await moviesController.GetAll(page: 3);
+            
+            // assert
+            Assert.IsType<NoContentResult>(result.Result);
         }
 
         [Fact]
